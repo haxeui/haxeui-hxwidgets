e79c4152aab35ddde9754ba74ad103d12f9bb5b9
Ian Harrigan <ianharrigan@hotmail.com>
2017-01-13 15:24:05 +0100

    implement password

---
+++ haxe/ui/backend/hxwidgets/behaviours/TextCtrlPassword.hx
@@ -0,0 +1,60 @@
+package haxe.ui.backend.hxwidgets.behaviours;
+
+import haxe.ui.core.UIEvent;
+import haxe.ui.util.Variant;
+import hx.widgets.TextCtrl;
+import hx.widgets.styles.TextCtrlStyle;
+
+@:access(haxe.ui.backend.ComponentBase)
+class TextCtrlPassword extends HxWidgetsBehaviour {
+    public override function set(value:Variant) {
+        super.set(value);
+        if (_component.window == null) {
+            return;
+        }
+
+        if (value.isNull) {
+            return;
+        }
+        
+        var style:Int = 0;
+        if (value == true) {
+            style |= TextCtrlStyle.PASSWORD;
+        }
+        
+        var eventMap:Map<String, UIEvent->Void> = copyMap(_component._eventMap);
+        for (k in eventMap.keys()) { // unmap any events on current window
+            _component.unmapEvent(k, eventMap.get(k));
+        }
+        
+        var oldControl:TextCtrl = cast(_component.window, TextCtrl);
+        var newControl:TextCtrl = new TextCtrl(oldControl.parent, oldControl.value, style);
+        newControl.move(oldControl.position.x, oldControl.position.y);
+        newControl.size = oldControl.size;
+        oldControl.destroy();
+        _component.window = newControl;
+        _component.invalidateStyle();
+        
+        for (k in eventMap.keys()) { // remap any events on current window
+            _component.mapEvent(k, eventMap.get(k));
+        }
+    }
+    
+    public override function get():Variant {
+        if (_component.window == null) {
+            return false;
+        }
+        
+        return (_component.window.windowStyle & TextCtrlStyle.PASSWORD) == TextCtrlStyle.PASSWORD;
+    }
+    
+    private function copyMap(map:Map<String, UIEvent->Void>):Map<String, UIEvent->Void> {
+        var newMap:Map<String, UIEvent->Void> = new Map<String, UIEvent->Void>();
+        
+        for (k in map.keys()) {
+            newMap.set(k, map.get(k));
+        }
+        
+        return newMap;
+    }
+}
