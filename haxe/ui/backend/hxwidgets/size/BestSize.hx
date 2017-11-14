211cecae319b875977213d8231dc515e14b4f7fa
Ian Harrigan <ianharrigan@hotmail.com>
2017-11-15 00:08:43 +0100

    fix wx word wrapping

--- haxe/ui/backend/hxwidgets/size/BestSize.hx
+++ haxe/ui/backend/hxwidgets/size/BestSize.hx
@@ -1,9 +1,12 @@
 package haxe.ui.backend.hxwidgets.size;
 
+import haxe.ui.backend.hxwidgets.custom.TransparentStaticText;
 import haxe.ui.layouts.DelegateLayout.DelegateLayoutSize;
 import hx.widgets.Dialog;
+import hx.widgets.HardBreakWrapper;
 import hx.widgets.StaticText;
 
+@:access(haxe.ui.core.Component)
 class BestSize extends DelegateLayoutSize {
     private override function get_width():Float {
         if (component.window == null) {
@@ -18,8 +21,12 @@ class BestSize extends DelegateLayoutSize {
         }
         
         if (Std.is(component.window, StaticText) && component.autoWidth == false && component.width > 0) {
-            var l:StaticText = cast component.window;
-            l.wrap(Std.int(component.width));
+            var label = component.get("originalLabel");
+            if (label != null) { // https://forums.wxwidgets.org/viewtopic.php?t=26973
+                var l:StaticText = cast component.window;
+                var h:HardBreakWrapper = new HardBreakWrapper(l, label, Std.int(component.width));
+                l.label = h.wrapped;
+            }
         }
         
         return component.window.bestSize.height;
