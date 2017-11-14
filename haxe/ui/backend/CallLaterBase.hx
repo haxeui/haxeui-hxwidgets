2e60c8a0c370d2cfbd85da04012ceefea8ace96c
Ian Harrigan <ianharrigan@hotmail.com>
2017-11-15 00:09:18 +0100

    change call later (may need to change back / investigate)

--- haxe/ui/backend/CallLaterBase.hx
+++ haxe/ui/backend/CallLaterBase.hx
@@ -1,6 +1,7 @@
 package haxe.ui.backend;
 
 import haxe.ui.core.Screen;
+import haxe.ui.util.Timer;
 import hx.widgets.EventType;
 import hx.widgets.ThreadEvent;
 
@@ -8,13 +9,26 @@ class CallLaterBase {
     private var _fn:Void->Void;
     
     public function new(fn:Void->Void) {
+        var t = null;
+        /*
+        t = new Timer(0, function() {
+            t.stop();
+            fn();
+        });
+        */
+        fn(); // actually works nicely like this - no flashing, could be a bottle neck though
+        
+        /* TODO: causes issues when resizing... obviously pauses main loop when resizing top level frame
         _fn = fn;
         Screen.instance.frame.bind(EventType.THREAD, onThreadEvent);
         Screen.instance.frame.queueEvent(new ThreadEvent());
+        */
     }
-    
+
+    /*
     private function onThreadEvent(e) {
         Screen.instance.frame.unbind(EventType.THREAD, onThreadEvent);
         _fn();
     }
+    */
 }
