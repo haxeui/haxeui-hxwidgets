9d98eb1a748dab97709ef117dbba7d89d8442ac6
Ian Harrigan <ianharrigan@hotmail.com>
2018-03-28 18:54:58 +0200

    clean up behaviour

--- haxe/ui/backend/hxwidgets/behaviours/ControlBitmap.hx
+++ haxe/ui/backend/hxwidgets/behaviours/ControlBitmap.hx
@@ -1,17 +1,9 @@
 package haxe.ui.backend.hxwidgets.behaviours;
 
-import haxe.Http;
-import haxe.io.Bytes;
 import haxe.ui.core.DataBehaviour;
-import haxe.ui.util.Variant;
 import haxe.ui.util.ImageLoader;
-import hx.widgets.Bitmap;
 import hx.widgets.Button;
-import hx.widgets.Control;
-import hx.widgets.Direction;
-import hx.widgets.Image;
 import hx.widgets.StaticBitmap;
-import hx.widgets.styles.ButtonStyle;
 
 class ControlBitmap extends DataBehaviour {
     public override function validateData() {
@@ -33,41 +25,3 @@ class ControlBitmap extends DataBehaviour {
         });
     }
 }
-
-/*
-class ControlBitmap extends HxWidgetsBehaviour {
-    public override function set(value:Variant) {
-        super.set(value);
-        if (_component.window == null) {
-            return;
-        }
-
-        if (value.isNull) {
-            return;
-        }
-
-        if (Std.is(_component.window, Button)) {
-            if (Resource.getBytes(value) != null) {
-                var button:Button = cast _component.window;
-                button.bitmap = Bitmap.fromHaxeResource(value);
-            }
-        } else if (Std.is(_component.window, StaticBitmap)) {
-            var imageLoader:ImageLoader = new ImageLoader(value);
-            imageLoader.load(function(imageInfo) {
-                if (imageInfo != null) {
-                    var bmp:StaticBitmap = cast _component.window;
-                    bmp.bitmap = imageInfo.data;
-                    if (bmp.parent != null) {
-                        bmp.parent.refresh(); // if bitmap has resized, get rid of any left of artifacts from parent (wx thang!)
-                    }
-                    _component.invalidateLayout();
-                }
-            });
-        }
-    }
-
-    public override function get():Variant {
-        return _value;
-    }
-}
-*/
