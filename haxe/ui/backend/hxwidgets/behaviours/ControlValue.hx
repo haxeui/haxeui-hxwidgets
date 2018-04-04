6f01543db3161647116198876b04706a9cc42221
Ian Harrigan <ianharrigan@hotmail.com>
2018-04-04 14:40:36 +0200

    use data behaviours

--- haxe/ui/backend/hxwidgets/behaviours/ControlValue.hx
+++ haxe/ui/backend/hxwidgets/behaviours/ControlValue.hx
@@ -1,5 +1,6 @@
 package haxe.ui.backend.hxwidgets.behaviours;
 
+import haxe.ui.core.DataBehaviour;
 import haxe.ui.util.Variant;
 import hx.widgets.CheckBox;
 import hx.widgets.Control;
@@ -9,36 +10,33 @@ import hx.widgets.ScrollBar;
 import hx.widgets.Slider;
 
 @:keep
-class ControlValue extends HxWidgetsBehaviour {
-    public override function set(value:Variant) {
-        super.set(value);
-        
-        if (_component.window == null) {
-            return;
-        }
-        
+class ControlValue extends DataBehaviour {
+    public override function validateData() {
         if (Std.is(_component.window, Gauge)) {
-            cast(_component.window, Gauge).value = value;
+            cast(_component.window, Gauge).value = _value;
         } else if (Std.is(_component.window, Slider)) {
-            cast(_component.window, Slider).value = value;
+            cast(_component.window, Slider).value = _value;
         } else if (Std.is(_component.window, ScrollBar)) {
             var scroll:ScrollBar = cast(_component.window, ScrollBar);
-            scroll.setScrollbar(value, scroll.thumbSize, scroll.range, scroll.pageSize);
+            scroll.setScrollbar(_value, scroll.thumbSize, scroll.range, scroll.pageSize);
         } else if (Std.is(_component.window, CheckBox)) {
-            cast(_component.window, CheckBox).value = value;
+            cast(_component.window, CheckBox).value = _value;
         } else if (Std.is(_component.window, RadioButton)) {
-            cast(_component.window, RadioButton).value = value;
+            cast(_component.window, RadioButton).value = _value;
         }
     }
-
+    
     public override function get():Variant {
-        if (Std.is(_component.window, RadioButton)) {
-            return cast(_component.window, RadioButton).value;
+        var v:Variant = null;
+        if (Std.is(_component.window, Gauge)) {
+            v = cast(_component.window, Gauge).value;
         } else if (Std.is(_component.window, Slider)) {
-            return cast(_component.window, Slider).value;
-        } else if (Std.is(_component.window, Gauge)) {
-            return cast(_component.window, Gauge).value;
+            v = cast(_component.window, Slider).value;
+        } else if (Std.is(_component.window, CheckBox)) {
+            v = cast(_component.window, CheckBox).value;
+        } else if (Std.is(_component.window, RadioButton)) {
+            v = cast(_component.window, RadioButton).value;
         }
-        return null;
+        return v;
     }
 }
