24d9e8a17d9aaec44db18dc6ca5db59b8707f57f
Ian Harrigan <ianharrigan@hotmail.com>
2019-12-04 15:05:56 +0100

    timer fix

--- haxe/ui/backend/TimerImpl.hx
+++ haxe/ui/backend/TimerImpl.hx
@@ -8,19 +8,29 @@ import hx.widgets.Timer;
 
 class TimerImpl {
     private var _timer:Timer;
+    private var _callback:Void->Void;
 
     public function new(delay:Int, callback:Void->Void) {
         if (Platform.isMac && delay == 0) {
             delay = 1;
         }
         var frame:Frame = Screen.instance.options.frame;
+        _callback = callback;
+        frame.bind(EventType.TIMER, onTimer);
         _timer = new Timer(frame, delay);
-        frame.bind(EventType.TIMER, function(e) {
-           callback();
-        });
     }
 
+    private function onTimer(e) {
+        if (_callback != null) {
+            _callback();
+        }
+    }
+    
     public function stop() {
+        _callback = null;
+        var frame:Frame = Screen.instance.options.frame;
+        frame.unbind(EventType.TIMER, onTimer);
         _timer.stop();
+        _timer.destroy();
     }
 }
