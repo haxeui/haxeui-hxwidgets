7d628b46d6cd2d95741888575c6f4aea90dcf73e
Ian Harrigan <ianharrigan@hotmail.com>
2021-04-19 19:33:15 +0200

    allow for shortcutText

--- haxe/ui/backend/hxwidgets/behaviours/MenuLabel.hx
+++ haxe/ui/backend/hxwidgets/behaviours/MenuLabel.hx
@@ -5,6 +5,11 @@ import hx.widgets.MenuItem;
 
 class MenuLabel extends DataBehaviour {
     public override function validateData() {
-        cast(_component.object, MenuItem).label = _value;
+        var item = cast(_component, haxe.ui.containers.menus.MenuItem);
+        var label:String = _value;
+        if (item.shortcutText != null) {
+            label += "\t" + item.shortcutText;
+        }
+        cast(_component.object, MenuItem).label = label;
     }
 }
