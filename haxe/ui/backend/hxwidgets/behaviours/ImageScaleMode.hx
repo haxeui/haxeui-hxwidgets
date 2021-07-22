43c90e1e7b90b81ae207e76c3deb7c061e2004a4
Ian Harrigan <ianharrigan@hotmail.com>
2021-07-22 10:46:55 +0200

    Image::scaleMode honoured

---
+++ haxe/ui/backend/hxwidgets/behaviours/ImageScaleMode.hx
@@ -0,0 +1,18 @@
+package haxe.ui.backend.hxwidgets.behaviours;
+
+import haxe.ui.backend.hxwidgets.custom.TransparentStaticBitmap;
+import haxe.ui.behaviours.DataBehaviour;
+import haxe.ui.util.Variant;
+
+class ImageScaleMode extends DataBehaviour {
+    public override function validateData() {
+        if (_component.window == null) {
+            return;
+        }
+      
+        var bitmap = cast(_component.window, TransparentStaticBitmap);
+        bitmap.scaleMode = _value;
+        
+        trace("validate: " + _value);
+    }
+}
--- haxe/ui/backend/hxwidgets/custom/TransparentStaticBitmap.hx
+++ haxe/ui/backend/hxwidgets/custom/TransparentStaticBitmap.hx
@@ -1,5 +1,6 @@
 package haxe.ui.backend.hxwidgets.custom;
 
+import haxe.ui.constants.ScaleMode;
 import hx.widgets.Bitmap;
 import hx.widgets.Event;
 import hx.widgets.EventType;
@@ -10,10 +11,28 @@ class TransparentStaticBitmap extends StaticBitmap {
     private var _hasDown:Bool = true;
     private var _hasUp:Bool = true;
     
+    public var scaleMode:ScaleMode = ScaleMode.FILL;
+    
     public function new(parent:Window, bitmap:Bitmap, id:Int = -1) {
         super(parent, bitmap, id);
         super.bind(EventType.LEFT_DOWN, doNothing);
         super.bind(EventType.LEFT_UP, doNothing);
+        super.bind(EventType.SIZE, onSize);
+    }
+    
+    private var _scaleMode:ScaleMode = ScaleMode.FILL;
+    private function get_scaledMode():ScaleMode {
+        return _scaleMode;
+    }
+    private function set_scaleMode(value:ScaleMode):ScaleMode {
+        if (_scaleMode == value) {
+            return value; 
+        }
+        
+        _scaleMode = value;
+        onSize(null);
+        
+        return value;
     }
     
     public override function bind(event:Int, fn:Event->Void, id:Int = -1) {
@@ -31,5 +50,47 @@ class TransparentStaticBitmap extends StaticBitmap {
     
     private function doNothing(e) {
         
+    }
+    
+    private function onSize(_) {
+        var s = this.size;
+        var cx = this.size.width;
+        var cy = this.size.height;
+        var bmp = this.bitmap;
+        var bmpCX = bmp.width;
+        var bmpCY = bmp.height;
+        
+        if (cx != bmpCX || cy != bmpCY) {
+            
+            var scaleW:Float = cx / bmpCX;
+            var scaleH:Float = cy / bmpCY;
+            
+            if (scaleMode != ScaleMode.FILL) {
+                var scale:Float;
+                switch (scaleMode) {
+                    case ScaleMode.FIT_INSIDE:
+                        scale = (scaleW < scaleH) ? scaleW : scaleH;
+                    case ScaleMode.FIT_OUTSIDE:
+                        scale = (scaleW > scaleH) ? scaleW : scaleH;
+                    case ScaleMode.FIT_WIDTH:
+                        scale = scaleW;
+                    case ScaleMode.FIT_HEIGHT:
+                        scale = scaleH;
+                    default:    //ScaleMode.NONE
+                        scale = 1;
+                }
+                
+                scaleW = scale;
+                scaleH = scale;
+            }
+            
+            var image = bmp.convertToImage();
+            var scaled = image.scale(Std.int(bmpCX * scaleW), Std.int(bmpCY * scaleH));
+            this.bitmap = new Bitmap(scaled);
+           
+            bmp.destroy();
+            image.destroy();
+            scaled.destroy();
+        }
     }
 }
