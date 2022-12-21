2033b43953fdb9d9679ae8888e6e11e6c596fcbb
Maud <maud.davout@gmail.com>
2022-12-21 10:22:27 +0100

    Making menuBar working on certain linuxes

--- haxe/ui/backend/AppImpl.hx
+++ haxe/ui/backend/AppImpl.hx
@@ -163,6 +163,7 @@ class AppImpl extends AppBase {
             _frame.center();
         }
         _frame.thaw();
+        if (_frame.menuBar != null )_frame.menuBar.show();
         dispatch(new AppEvent(AppEvent.APP_STARTED));
         @:privateAccess TimerImpl.processDeferredTimers();
         _app.run();
