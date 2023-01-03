17da92c1bfe46e7d95799a4c7336f13ccb6dcd0f
Maud <maud.davout@gmail.com>
2023-01-04 00:34:23 +0100

    adding htmlText

---
+++ haxe/ui/backend/hxwidgets/behaviours/ControlMarkupLabel.hx
@@ -0,0 +1,25 @@
+package haxe.ui.backend.hxwidgets.behaviours;
+
+import haxe.ui.behaviours.DataBehaviour;
+import hx.widgets.Control;
+
+@:access(haxe.ui.core.Component)
+class ControlMarkupLabel extends DataBehaviour {
+    
+    public override function validateData() {
+        var ctrl:Control = cast(_component.window, Control);
+        if (_value != null) {
+            _component.set("originalLabel", _value.toString()); // for wrapping, see: haxe.ui.backend.hxwidgets.size.BestSize
+            ctrl.setLabelMarkup(normalizeText(_value));
+            _component.invalidateComponentLayout();
+            _component.invalidateComponentStyle();
+        } else {
+            _component.set("originalLabel", null);
+        }
+    }
+    
+    private inline function normalizeText(s:String) {
+        s = StringTools.replace(s, "\\n", "\r\n");
+        return s;
+    }
+}
